8a52364648946f1278f3bfd97920adf98031dabb
Kyosohee18 <hanashahky@gmail.com>
2025-05-26 16:10:33 +0700

    int main pelajar

--- inheritance/inherintance.cpp
+++ inheritance/inherintance.cpp
@@ -32,4 +32,10 @@ public:
 }
  };
  
+int main (){
+    pelajar siswa1("andi laksono", "SMAN 1 BANTU");
+    cout << siswa1.perkenalan();
+    cout <<"Hasil = " << siswa1.jumlah(10,90) <<endl;
 
+    return 0;
+}
