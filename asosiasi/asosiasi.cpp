dc235d2e6b45c5c5fce426a438a0ed4047fc5475
Kyosohee18 <hanashahky@gmail.com>
2025-05-26 11:32:14 +0700

    relasidanInheritance_0046

---
+++ asosiasi/asosiasi.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+// nama repo
+//relasidanInheritamce_4nim belakang
+
+
+class dokter;
+class pasien { //asosasi
+public:
+    string nama;
+    vector<dokter*> daftar_dokter;
+
+
+
+
+}
